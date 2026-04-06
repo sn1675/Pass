bed8a340d69e6d6034d4092499bab0dc8c1e0b34
sn1675 <nathcourbet@gmail.com>
2026-04-06 16:06:34 +0200

    UI/UX

--- build/moc_mainwindow.cpp
+++ build/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[4];
-    char stringdata0[44];
+    QByteArrayData data[7];
+    char stringdata0[104];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,10 +35,15 @@ static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 14), // "onLoginClicked"
 QT_MOC_LITERAL(2, 26, 0), // ""
-QT_MOC_LITERAL(3, 27, 16) // "onAccountClicked"
+QT_MOC_LITERAL(3, 27, 16), // "onAccountClicked"
+QT_MOC_LITERAL(4, 44, 20), // "onAddPasswordClicked"
+QT_MOC_LITERAL(5, 65, 18), // "onSaveEntryClicked"
+QT_MOC_LITERAL(6, 84, 19) // "onBackToHomeClicked"
 
     },
-    "MainWindow\0onLoginClicked\0\0onAccountClicked"
+    "MainWindow\0onLoginClicked\0\0onAccountClicked\0"
+    "onAddPasswordClicked\0onSaveEntryClicked\0"
+    "onBackToHomeClicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -48,7 +53,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -56,10 +61,16 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x08 /* Private */,
-       3,    0,   25,    2, 0x08 /* Private */,
+       1,    0,   39,    2, 0x08 /* Private */,
+       3,    0,   40,    2, 0x08 /* Private */,
+       4,    0,   41,    2, 0x08 /* Private */,
+       5,    0,   42,    2, 0x08 /* Private */,
+       6,    0,   43,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -74,6 +85,9 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->onLoginClicked(); break;
         case 1: _t->onAccountClicked(); break;
+        case 2: _t->onAddPasswordClicked(); break;
+        case 3: _t->onSaveEntryClicked(); break;
+        case 4: _t->onBackToHomeClicked(); break;
         default: ;
         }
     }
@@ -109,13 +123,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 5;
     }
     return _id;
 }
